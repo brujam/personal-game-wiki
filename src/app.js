0921bc335659759f8ad3afe48d866d7343a98b1e
J. Bruce <jamesjbruce@gmail.com>
2016-10-09 22:00:45 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ function dump_db() {
   console.log("Begin dump; \n---\n");
   window.db.find({}, function(err, doc) {
     doc.forEach(function(document) {
-        console.log("db.insert(" + JSON.stringify(document) + ")")
+        console.log("db.insert(" + JSON.stringify(document) + ");")
     })
   });
   window.setTimeout(function () {console.log("\n---\nEnd dump;");}, 1000);
